142a4a7061e4ad26292746072f3caaca2d0f3e12
gbressan <gbressandev@gmail.com>
2024-02-12 00:57:44 -0300

    Order page

---
+++ src/app/order/page.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "@chakra-ui/react";
+import React from "react";
+
+const Order: React.FC = function ({}) {
+  return <Stack spacing="0px"></Stack>;
+};
+
+export default Order;
