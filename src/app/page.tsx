c431feea83d9e4d64d2beaccb60df45f70fe6e42
IcaroAndries <icaro.dev.andries@gmail.com>
2024-02-13 00:05:33 -0300

    finishing about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 import Hero from "~/modules/components/Hero";
 import Footer from "~/modules/components/Footer";
+import About from "~/modules/components/About";
 
 const Home: React.FC = function ({}) {
   return (
     <Stack spacing="0px" h="100%">
       <Hero />
+      <About />
       <Footer />
     </Stack>
   );
